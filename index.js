564961406d934a6607659c8857fdfff9ce2e690e
Rob Paik <robpaik@gmail.com>
2019-06-13 02:45:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ function sayHiToGrandma(string)  {
   else if (string) === uppercase  {
     return "YES INDEED!"
   }
+  else if (string) === "I love you, Grandma."  {
+    return "I love you, too."
+  }
 }
