e6ea82aea71645ae38811da6ec257d0fbf833767
Rob Paik <robpaik@gmail.com>
2019-06-13 02:44:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,10 @@ function logWhisper(string)  {
 }
 
 function sayHiToGrandma(string)  {
-  if (string)
+  if (string) === lowercase {
+    return "I can't hear you!"
+  }
+  else if (string) === uppercase  {
+    return "YES INDEED!"
+  }
 }
